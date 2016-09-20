100549a824746763cf07b9b6028e08190288ef49
rudis <rudislpp@gmail.com>
2016-09-20 13:13:06 -0300

    reset button

--- js.js
+++ js.js
@@ -3,22 +3,43 @@ var p2Button = document.getElementById('p2');
 var p1Display = document.querySelector('#p1Display');
 var p2Display = document.querySelector('#p2Display');
 
-var reset = document.getElementById('reset');
-
 var p1Score = 0;
 var p2Score = 0;
-p1Buttuon.addEventListener("click", function() {
-    p1Score++;
-    p1Display.innerHTML = p1Score;
 
+var gameOver = false;
+var winnigScore = 5;
+
+var reset = document.getElementById('reset');
+var pWinner = document.getElementById('pWinner');
+
+p1Buttuon.addEventListener("click", function() {
+    if (!gameOver) {
+        p1Score++;
+        if (p1Score === winnigScore) {
+            gameOver = true;
+            pWinner.innerHTML = "Player 1 has Won! Gratz!";
+        }
+        p1Display.innerHTML = p1Score;
+    }
 });
 
 p2Button.addEventListener("click", function() {
-    p2Score++
+    if (!gameOver) {
+        p2Score++
+    }
+    if (p2Score === winnigScore) {
+        gameOver = true;
+        pWinner.innerHTML = "Player 2 has Won! Gratz!";
+    }
     p2Display.innerHTML = p2Score;
 })
 
 
-reset.addEventListener("click", function(){
-    clear(p1Score,p2Score);
+reset.addEventListener("click", function() {
+    gameOver = false;
+    p1Display.innerHTML = " ";
+    p2Display.innerHTML = " ";
+    p1Score = 0;
+    p2Score = 0;
+    pWinner.innerHTML = " ";
 })
